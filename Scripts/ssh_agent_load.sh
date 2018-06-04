3bd23d6e0f996a3e8404f15b5b47cf6c92c9d2ad
Erasmus Cedernaes <erasmus.cedernaes@gmail.com>
2018-06-04 20:22:24 +0200

    Adding error check

--- Scripts/ssh_agent_load.sh
+++ Scripts/ssh_agent_load.sh
@@ -2,7 +2,7 @@
 
 # Attempt to find old agents
 find_ssh_agents () {
-  find /tmp/ssh* -name "agent.*" |
+  find /tmp/ssh* -name "agent.*" 2>/dev/null |
     while read f; do
       echo "$f";
     done
@@ -14,6 +14,11 @@ if [ `find_ssh_agents | wc -l` -gt 0 ]; then
   # Recover the first one's ID and file handle
   SSH_AUTH_SOCK=`find_ssh_agents | head -1`
   SSH_AGENT_ID=$((`echo "$SSH_AUTH_SOCK" | grep -Po "\\d+$"` + 1))
+
+  if [ -z "`ps --pid $SSH_AGENT_ID | grep 'ssh-agent'`" ]; then
+    echo "Could not find the process corresponding to the agent..."
+    echo "Something might be wrong."
+  fi
 
   echo "Restoring agent $SSH_AGENT_ID ($SSH_AUTH_SOCK)"
 else
