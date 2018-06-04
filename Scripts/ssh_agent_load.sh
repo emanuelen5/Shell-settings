a7d4c5a123a44c0ad1963bfe87f3e50de17afd78
Erasmus Cedernaes <erasmus.cedernaes@gmail.com>
2018-06-04 19:12:28 +0200

    Fix SSH key addition for Ubuntu
    - On my virtual machine with Ubuntu 16, $SSH_AUTH_SOCK is
      initialized to "/run/user/1000/keyring/ssh", which made the
      check for empty fail.

--- Scripts/ssh_agent_load.sh
+++ Scripts/ssh_agent_load.sh
@@ -1,7 +1,8 @@
 #!/bin/sh
 
 ## Start the SSH agent
-if [ -z "$SSH_AUTH_SOCK" ] ; then
+SSH_AUTH_SOCK_NAME=`echo $SSH_AUTH_SOCK | grep -Po 'agent\.\d+$'`
+if [ -z "$SSH_AUTH_SOCK" ] || [ -z "$SSH_AUTH_SOCK_NAME" ] ; then
     eval `ssh-agent -s` >& /dev/null
     ssh-add
 fi
