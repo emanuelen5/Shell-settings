b4eeb974856208010fa3f99240d8f13eab6ca11a
Erasmus Cedernaes <erasmus.cedernaes@gmail.com>
2018-07-04 09:36:25 +0200

    Adding some comments on usage

--- Scripts/cd_enter_exit.sh
+++ Scripts/cd_enter_exit.sh
@@ -1,9 +1,13 @@
+# Runs specific scripts in folder upon enter and exit
 function cd() {
   test -r .exit.sh && . .exit.sh
+
+  # Must special treat "no argument" case
   if [ $# = 0 ]; then
     builtin cd
   else
     builtin cd "$1"
   fi
+
   test -r .enter.sh && . .enter.sh
 }
