c22924649912122020b54bd190ca2df9938793ee
Erasmus Cedernaes <erasmus.cedernaes@gmail.com>
2018-07-06 23:04:12 +0200

    Fixing exit code when scripts do not exist

--- Scripts/cd_enter_exit.sh
+++ Scripts/cd_enter_exit.sh
@@ -1,6 +1,8 @@
 # Runs specific scripts in folder upon enter and exit
 function cd() {
-  test -r .exit.sh && . .exit.sh
+  if [ -r .exit.sh ]; then
+    . .exit.sh
+  fi
 
   # Must special treat "no argument" case
   if [ $# = 0 ]; then
@@ -9,5 +11,7 @@ function cd() {
     builtin cd "$1"
   fi
 
-  test -r .enter.sh && . .enter.sh
+  if [ -r .enter.sh ]; then
+    . .enter.sh
+  fi
 }
