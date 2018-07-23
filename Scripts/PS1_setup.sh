363f7d3c8359838f65769a4d8458f0a7dc5f1afa
Erasmus Cedernaes <erasmus.cedernaes@gmail.com>
2018-07-24 00:04:26 +0200

    Adding explanation for the colorings and removing redundant variables

--- Scripts/PS1_setup.sh
+++ Scripts/PS1_setup.sh
@@ -1,14 +1,32 @@
-# Taken from: http://mediadoneright.com/content/ultimate-git-ps1-bash-prompt
-
-# Customize BASH PS1 prompt to show current GIT repository and branch.
-# by Mike Stewart - http://MediaDoneRight.com
-
-# SETUP CONSTANTS
-# Bunch-o-predefined colors.  Makes reading code easier than escape sequences.
-# I don't remember where I found this.  o_O
+## Ansi escape codes taken from http://www.termsys.demon.co.uk/vtansi.htm#colors
+# Set attribute mode      <ESC>[{attr1};...;{attrn}m
+# Attributes: 
+#   0   Reset all attributes
+#   1   Bright
+#   2   Dim
+#   4   Underscore
+#   5   Blink
+#   7   Reverse
+#   8   Hidden
+# Basic colors (for foreground and backgrounds with base number 'n'. E.g. <ESC>[31m for red foreground):
+#   n+0  Black
+#   n+1  Red
+#   n+2  Green
+#   n+3  Yellow
+#   n+4  Blue
+#   n+5  Magenta
+#   n+6  Cyan
+#   n+7  White
+#   n+8;2;R;G;B  RGB-color (e.g. <ESC>[38;2;255;0;0m for red foreground)
+# Foreground colors (starts at 30)
+# Background colors (starts at 40)
+# Bright foreground (starts at 90)
+# Bright background (starts at 100)
 
 # Reset
-Color_Off="\e[0m"       # Text Reset
+Color_Off="\e[0m"
+Color_Underscore="\e[4m"
+Color_Reverse="\e[7m"
 
 # Regular Colors
 Black="\e[0;30m"        # Black
@@ -19,38 +37,16 @@ Blue="\e[0;34m"         # Blue
 Purple="\e[0;35m"       # Purple
 Cyan="\e[0;36m"         # Cyan
 White="\e[0;37m"        # White
-
-# Bold
-BBlack="\e[1;30m"       # Black
-BRed="\e[1;31m"         # Red
-BGreen="\e[1;32m"       # Green
-BYellow="\e[1;33m"      # Yellow
-BBlue="\e[1;34m"        # Blue
-BPurple="\e[1;35m"      # Purple
-BCyan="\e[1;36m"        # Cyan
-BWhite="\e[1;37m"       # White
-
-# Underline
-UBlack="\e[4;30m"       # Black
-URed="\e[4;31m"         # Red
-UGreen="\e[4;32m"       # Green
-UYellow="\e[4;33m"      # Yellow
-UBlue="\e[4;34m"        # Blue
-UPurple="\e[4;35m"      # Purple
-UCyan="\e[4;36m"        # Cyan
-UWhite="\e[4;37m"       # White
-
 # Background
-On_Black="\e[40m"       # Black
-On_Red="\e[41m"         # Red
-On_Green="\e[42m"       # Green
-On_Yellow="\e[43m"      # Yellow
-On_Blue="\e[44m"        # Blue
-On_Purple="\e[45m"      # Purple
-On_Cyan="\e[46m"        # Cyan
-On_White="\e[47m"       # White
-
-# High Intensty
+On_Black="\e[0;40m"       # Black
+On_Red="\e[0;41m"         # Red
+On_Green="\e[0;42m"       # Green
+On_Yellow="\e[0;43m"      # Yellow
+On_Blue="\e[0;44m"        # Blue
+On_Purple="\e[0;45m"      # Purple
+On_Cyan="\e[0;46m"        # Cyan
+On_White="\e[0;47m"       # White
+# Bright foregrounds 
 IBlack="\e[0;90m"       # Black
 IRed="\e[0;91m"         # Red
 IGreen="\e[0;92m"       # Green
@@ -59,18 +55,7 @@ IBlue="\e[0;94m"        # Blue
 IPurple="\e[0;95m"      # Purple
 ICyan="\e[0;96m"        # Cyan
 IWhite="\e[0;97m"       # White
-
-# Bold High Intensty
-BIBlack="\e[1;90m"      # Black
-BIRed="\e[1;91m"        # Red
-BIGreen="\e[1;92m"      # Green
-BIYellow="\e[1;93m"     # Yellow
-BIBlue="\e[1;94m"       # Blue
-BIPurple="\e[1;95m"     # Purple
-BICyan="\e[1;96m"       # Cyan
-BIWhite="\e[1;97m"      # White
-
-# High Intensty backgrounds
+# Bright backgrounds
 On_IBlack="\e[0;100m"   # Black
 On_IRed="\e[0;101m"     # Red
 On_IGreen="\e[0;102m"   # Green
@@ -93,26 +78,6 @@ ePurple="\[${Purple}\]"         # Purple
 eCyan="\[${Cyan}\]"             # Cyan
 eWhite="\[${White}\]"           # White
 
-# Bold
-eBBlack="\[${BBlack}\]"         # Black
-eBRed="\[${BRed}\]"             # Red
-eBGreen="\[${BGreen}\]"         # Green
-eBYellow="\[${BYellow}\]"       # Yellow
-eBBlue="\[${BBlue}\]"           # Blue
-eBPurple="\[${BPurple}\]"       # Purple
-eBCyan="\[${BCyan}\]"           # Cyan
-eBWhite="\[${BWhite}\]"         # White
-
-# Underline
-eUBlack="\[${UBlack}\]"         # Black
-eURed="\[${URed}\]"             # Red
-eUGreen="\[${UGreen}\]"         # Green
-eUYellow="\[${UYellow}\]"       # Yellow
-eUBlue="\[${UBlue}\]"           # Blue
-eUPurple="\[${UPurple}\]"       # Purple
-eUCyan="\[${UCyan}\]"           # Cyan
-eUWhite="\[${UWhite}\]"         # White
-
 # Background
 eOn_Black="\[${On_Black}\]"     # Black
 eOn_Red="\[${On_Red}\]"         # Red
@@ -133,16 +98,6 @@ eIPurple="\[${IPurple}\]"       # Purple
 eICyan="\[${ICyan}\]"           # Cyan
 eIWhite="\[${IWhite}\]"         # White
 
-# Bold High Intensty
-eBIBlack="\[${BIBlack}\]"       # Black
-eBIRed="\[${BIRed}\]"           # Red
-eBIGreen="\[${BIGreen}\]"       # Green
-eBIYellow="\[${BIYellow}\]"     # Yellow
-eBIBlue="\[${BIBlue}\]"         # Blue
-eBIPurple="\[${BIPurple}\]"     # Purple
-eBICyan="\[${BICyan}\]"         # Cyan
-eBIWhite="\[${BIWhite}\]"       # White
-
 # High Intensty backgrounds
 eOn_IBlack="\[${On_IBlack}\]"   # Black
 eOn_IRed="\[${On_IRed}\]"       # Red
@@ -153,6 +108,7 @@ eOn_IPurple="\[${On_IPurple}\]" # Purple
 eOn_ICyan="\[${On_ICyan}\]"     # Cyan
 eOn_IWhite="\[${On_IWhite}\]"   # White
 
+# Taken from: http://mediadoneright.com/content/ultimate-git-ps1-bash-prompt
 # Various variables you might want for your PS1 prompt instead
 # see https://ss64.com/bash/syntax-prompt.html
 Date="\d" # The data, in "Weekday Month Date" format (e.g., "Tue May 26")
