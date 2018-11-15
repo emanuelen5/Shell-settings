46133fa5f51cb536a6822061c2dfb327e19b7254
Erasmus Cedernaes <erasmus.cedernaes@gmail.com>
2018-11-15 11:19:18 +0100

    Adding helping printouts

--- Scripts/create_venv.sh
+++ Scripts/create_venv.sh
@@ -16,4 +16,12 @@ cp ~/Shell-settings/Scripts/enter_exit_template/.{enter,exit}.sh .
 PYTHON_EXECUTABLE=`which python`
 PROMPT="($1) "
 
-virtualenv --python="${PYTHON_EXECUTABLE}" --prompt="${PROMPT}" venv
+echo "Setting python prompt to \"${PROMPT}\""
+echo "Using executable on path \"${PYTHON_EXECUTABLE}\""
+echo "Creating virtual environment in ${PWD}/venv"
+echo -n "Installing..."
+
+virtualenv --python="${PYTHON_EXECUTABLE}" --prompt="${PROMPT}" venv >/dev/null
+echo " Done!"
+echo ""
+echo "Don't forget to activate the virtual environment (i.e. \`source .enter.sh\`)!"
