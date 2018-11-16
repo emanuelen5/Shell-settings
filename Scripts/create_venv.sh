af71863683167f713e52d63ad9935aeffc1e3105
Erasmus Cedernaes <erasmus.cedernaes@saabgroup.com>
2018-11-16 10:06:10 +0100

    Checking that enter/exit scripts exist

--- Scripts/create_venv.sh
+++ Scripts/create_venv.sh
@@ -12,7 +12,14 @@ fi
 
 venv_name="$1"
 
-cp ~/Shell-settings/Scripts/enter_exit_template/.{enter,exit}.sh .
+# Activate venv upon entering the directory
+enter_exit_template_folder="~/Shell-settings/Scripts/enter_exit_template"
+if [ -d $enter_exit_template_folder ]; then
+  cp $enter_exit_template_folder/.{enter,exit}.sh .
+else
+  echo "Could not find enter/exit scripts in '$enter_exit_template_folder'"
+fi
+
 PYTHON_EXECUTABLE=`which python`
 PROMPT="($1) "
 
@@ -24,4 +31,4 @@ echo -n "Installing..."
 virtualenv --python="${PYTHON_EXECUTABLE}" --prompt="${PROMPT}" venv >/dev/null
 echo " Done!"
 echo ""
-echo "Don't forget to activate the virtual environment (i.e. \`source .enter.sh\`)!"
+echo "Don't forget to activate the virtual environment (i.e. \`source .enter.sh\`, using the enter/exit scripts)!"
